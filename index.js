4a842ae24b27d3de963c67aff5ce516e6a65ed19
alisonarmstrong <alarmstrong15@gmail.com>
2021-04-15 11:01:31 -0600

    added lisence info

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const promptUser = () => {
             type: 'list',
             message: 'What type of lisence will you use?',
             name: 'lisence',
-            choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]  
+            choices: ["MIT", "APACHE 2.0", "None"]  
         },
         {
             type: 'input',
@@ -54,15 +54,51 @@ function renderLicenseBadge(license) {
         return `![MIT](https://img.shields.io/badge/License%20-MIT-brightgreen)`
     }
     else if (license === "APACHE 2.0") {
-        return `![APACHE 2.0] ()`
+        return `![APACHE 2.0] (https://img.shields.io/badge/License%20-Apache%202.0-blue)`
+    }
+    else if (lisence === "None") {
+        return `![None] (https://img.shields.io/badge/License%20-None-red)`
     }
 }
-function renderLicenseLink(license) {}
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+    if (license === "MIT") {
+        return `[MIT](https://www.mit.edu/~amini/LICENSE.md)`
+    }
+    else if (license === "APACHE 2.0") {
+        return `[APACHE 2.0] (https://www.apache.org/licenses/LICENSE-2.0)`
+    }
+    else if (lisence === "None") {
+        return `[None]`
+    }
+}
+function renderLicenseSection(license) {
+    if (license === "MIT") {
+        return `!###### The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology`
+    }
+    else if (license === "APACHE 2.0") {
+        return `###### The 2.0 version of the Apache License, approved by the ASF in 2004, helps us achieve our goal of providing reliable and long-lived software products through collaborative open source software development.`
+    }
+    else if (lisence === "None") {
+        return `![None]`
+    }
+}
+
 const generateReadMe = (answers) => {
 return `# Project Name: ${answers.title}
 ## Description 
-${answers.description}`};
+${answers.description}
+## Installation
+${answers.installation}
+## Technologies
+${answers.technologies}
+## Usage
+${answers.usage}
+## Lisence
+${answers.lisence}
+## Contributors
+${answers.contributors}
+## Tests
+${answers.tests}`};
 
 const init = () => {
     promptUser ()
